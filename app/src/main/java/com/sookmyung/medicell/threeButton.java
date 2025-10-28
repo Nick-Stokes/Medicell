71b9d8542a855289f283728f116c3ce3bf029c95
Nick-Stokes <133878161+Nick-Stokes@users.noreply.github.com>
2025-10-28 15:46:18 +0900

    버튼 화면 추가

--- app/src/main/java/com/sookmyung/medicell/threeButton.java
+++ app/src/main/java/com/sookmyung/medicell/threeButton.java
@@ -1,8 +1,6 @@
 package com.sookmyung.medicell;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class threeButton extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_three_button);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,14 +22,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        if(event.getAction()==MotionEvent.ACTION_DOWN){
-            Intent intent = new Intent(MainActivity.this, threeButton.class);
-            startActivity(intent);
-            finish();
-            return true;
-        }
-        return super.onTouchEvent(event);
-    }
+
 }
